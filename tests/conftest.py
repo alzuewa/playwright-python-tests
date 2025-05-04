6b7c120eae7f30ba002856efb58d6b034c13ba50
Albina <albizuewa@gmail.com>
2025-05-04 16:10:54 -0700

    Move browser and page initialisation into conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from playwright.sync_api import Page, Playwright
+
+
+@pytest.fixture(scope='session')
+def initialize_browser_state(playwright: Playwright) -> None:
+    chromium_browser = playwright.chromium.launch(headless=False)
+    context = chromium_browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email_field = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_field.fill('user.name@gmail.com')
+    username_field = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_field.fill('username')
+    password_field = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_field.fill('password')
+    password_field.click()
+
+    register_button = page.get_by_test_id('registration-page-registration-button')
+    register_button.click()
+
+    context.storage_state(path='browser-state.json')
+    yield
+    chromium_browser.close()
+
+
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    chromium_browser = playwright.chromium.launch(headless=False)
+    context = chromium_browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+    yield page
