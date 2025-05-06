ddc83a59969292a8ad6d46b678cecd4ed470aa03
Albina <albizuewa@gmail.com>
2025-05-05 20:23:34 -0700

    Rewrite test for authorization with POM

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,6 @@
 import pytest
-from playwright.sync_api import Page, expect
+
+from pages.login_page import LoginPage
 
 
 @pytest.mark.parametrize(
@@ -9,18 +10,7 @@ from playwright.sync_api import Page, expect
         ('  ', 'password')
     ]
 )
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-    chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
-
-    email_field = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_field.fill(email)
-
-    password_field = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_field.fill(password)
-
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
-
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.open('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    login_page.fill_in_login_form(email=email, password=password)
+    login_page.assert_wrong_creds_alert_visible()
