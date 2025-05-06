587d60db9cf121ff257d53fd78ea4d41e89875bc
Albina <albizuewa@gmail.com>
2025-05-05 22:59:57 -0700

    Reduce page fixtures to one code line

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -8,17 +8,14 @@ from pages.registration_page import RegistrationPage
 
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
-    login_page = LoginPage(page=chromium_page)
-    return login_page
+    return LoginPage(page=chromium_page)
 
 
 @pytest.fixture
 def registration_page(chromium_page: Page) -> RegistrationPage:
-    registration_page = RegistrationPage(page=chromium_page)
-    return registration_page
+    return RegistrationPage(page=chromium_page)
 
 
 @pytest.fixture
 def dashboard_page(chromium_page: Page) -> DashboardPage:
-    dashboard_page = DashboardPage(page=chromium_page)
-    return dashboard_page
+    return DashboardPage(page=chromium_page)
