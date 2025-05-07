b2600ee66f1de81d49c3346b2c5ab418f5448ff7
Albina <albizuewa@gmail.com>
2025-05-06 18:19:43 -0700

    Update dashboard page

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -9,7 +9,35 @@ class DashboardPage(BasePage):
         super().__init__(page=page)
 
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        self.students_chart_title = page.get_by_test_id('students-chart-title-text')
+        self.students_chart = page.get_by_test_id('students-bar-chart')
+        self.activities_chart_title = page.get_by_test_id('activities-chart-title-text')
+        self.activities_chart = page.get_by_test_id('activities-line-chart')
+        self.courses_chart_title = page.get_by_test_id('courses-chart-title-text')
+        self.courses_chart = page.get_by_test_id('courses-pie-chart')
+        self.scores_chart_title = page.get_by_test_id('scores-chart-title-text')
+        self.scores_chart = page.get_by_test_id('scores-scatter-chart')
 
     def assert_dashboard_title_visible(self):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text('Dashboard')
+
+    def assert_students_chart_visible(self):
+        expect(self.students_chart).to_be_visible()
+        expect(self.students_chart_title).to_be_visible()
+        expect(self.students_chart_title).to_have_text('Students')
+
+    def assert_activities_chart_visible(self):
+        expect(self.activities_chart).to_be_visible()
+        expect(self.activities_chart_title).to_be_visible()
+        expect(self.activities_chart_title).to_have_text('Activities')
+
+    def assert_courses_chart_visible(self):
+        expect(self.courses_chart).to_be_visible()
+        expect(self.courses_chart_title).to_be_visible()
+        expect(self.courses_chart_title).to_have_text('Courses')
+
+    def assert_scores_chart_visible(self):
+        expect(self.scores_chart).to_be_visible()
+        expect(self.scores_chart_title).to_be_visible()
+        expect(self.scores_chart_title).to_have_text('Scores')
