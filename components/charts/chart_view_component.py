e99b22af7de7ef6094bdcca249801a7c0cb93d31
Albina <albizuewa@gmail.com>
2025-05-09 08:41:47 -0700

    Remove unused import

--- components/charts/chart_view_component.py
+++ components/charts/chart_view_component.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
 from elements.image import Image
