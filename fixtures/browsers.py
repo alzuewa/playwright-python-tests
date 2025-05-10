6760b9359517851b15c5c2dcb9f895766de321af
Albina <albizuewa@gmail.com>
2025-05-09 22:36:29 -0700

    Apply PageFactory to fixtures

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import Page, Playwright
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
@@ -15,19 +17,11 @@ def initialize_browser_state(playwright: Playwright) -> None:
     context = chromium_browser.new_context()
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page = RegistrationPage(page=page)
 
-    email_field = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_field.fill('user.name@gmail.com')
-    username_field = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_field.fill('username')
-    password_field = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_field.fill('password')
-    password_field.click()
-
-    register_button = page.get_by_test_id('registration-page-registration-button')
-    register_button.click()
-
+    registration_page.open('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_register_button()
     context.storage_state(path='browser-state.json')
     chromium_browser.close()
 
