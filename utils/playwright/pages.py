2ae35db89109626374a1c9ae48af07f60793c529
Albina <albizuewa@gmail.com>
2025-05-13 06:29:14 -0700

    Parametrize browser fixtures

--- utils/playwright/pages.py
+++ utils/playwright/pages.py
@@ -2,11 +2,16 @@ import allure
 from allure_commons.types import AttachmentType
 from playwright.sync_api import Page, Playwright
 
-from config import settings
+from config import Browser, settings
 
 
-def initialize_playwright_page(playwright: Playwright, test_name: str, storage_state: str | None = None) -> Page:
-    browser = playwright.chromium.launch(headless=settings.headless)
+def initialize_playwright_page(
+        playwright: Playwright,
+        test_name: str,
+        browser_type: Browser,
+        storage_state: str | None = None
+) -> Page:
+    browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(
         base_url=settings.get_base_url(), record_video_dir=settings.videos_dir, storage_state=storage_state
     )
